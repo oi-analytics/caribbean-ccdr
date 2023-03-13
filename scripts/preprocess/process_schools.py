4387ea95918890b133b8fd447ab3bdb5ff2697df
Raghav Pant <raghavp@gmail.com>
2023-03-13 15:25:54 +0000

    school enrollment mapping

--- scripts/preprocess/process_schools.py
+++ scripts/preprocess/process_schools.py
@@ -42,9 +42,10 @@ def nearest_name(x,nodes,name_column):
             nearest_name = g[name_column]
             nearest_id = g["ID"]
             nearest_district = g["District"]
+            enrollment = g["Enrollment"]
         if highest_match == 1:
             break
-    return nearest_id,nearest_name,nearest_district,highest_match
+    return nearest_id,nearest_name,nearest_district,enrollment,highest_match
 
 def main(config):
     caribbean_epsg = 32620
@@ -52,6 +53,7 @@ def main(config):
     incoming_data_path = config['paths']['incoming_data']
     processed_data_path = config['paths']['data']
 
+    match_threshold = 0.78
     schools = gpd.read_file(os.path.join(processed_data_path,
                         "infrastructure/social",
                         "education.gpkg"),
@@ -59,11 +61,12 @@ def main(config):
     schools = schools.to_crs(epsg=caribbean_epsg)
     schools["area"] = schools.geometry.area
     
-    countries = ['GRD','LCA']
-    school_matches = []
+    countries = ['DMA','GRD','LCA','VCT']
+    all_schools = []
+    all_districts = []
     for country in countries:
         country_schools = schools[schools['iso_code'] == country]
-        print (country_schools)
+        # print (country_schools)
         if country == "LCA":
             admin_areas = gpd.read_file(os.path.join(
                                 processed_data_path,
@@ -73,38 +76,104 @@ def main(config):
                             layer="areas")[["DIS","geometry"]]
             admin_areas["DIS"] = admin_areas.progress_apply(lambda x: str(num2words(x["DIS"])).title(),axis=1)
             admin_areas.rename(columns={"DIS":"school_district"},inplace=True)
+        elif country == "VCT":
+            admin_areas = gpd.read_file(os.path.join(
+                                processed_data_path,
+                                "infrastructure",
+                                "social",
+                                "schools_districts_VCT.gpkg"))[["SCHOOL_DIST","geometry"]]
+            admin_areas["SCHOOL_DIST"] = admin_areas.progress_apply(lambda x: str(num2words(x["SCHOOL_DIST"])).title(),axis=1)
+            admin_areas.rename(columns={"SCHOOL_DIST":"school_district"},inplace=True)
         else:
             admin_areas = gpd.read_file(os.path.join(processed_data_path,
                                 "admin_boundaries",
                                 f"gadm41_{country}.gpkg"),layer="ADM_ADM_1")[["NAME_1","geometry"]]
+            admin_areas["NAME_1"] = admin_areas["NAME_1"].str.replace("Carriacou","Carriacou & Petite Martinique")
             admin_areas.rename(columns={"NAME_1":"school_district"},inplace=True)
         admin_areas = admin_areas.to_crs(epsg=caribbean_epsg)
         admin_areas["school_district"] = admin_areas["school_district"].astype(str).str.replace("Saint","St.")
         country_schools = match_buildings_to_adminarea(country_schools,"node_id",admin_areas,"school_district")
-        print (country_schools)
+        # print (country_schools)
 
         country_schools["name"] = country_schools["name"].astype(str).str.replace(" School","")
         country_schools["name"] = country_schools["name"].astype(str).str.replace(" school","")
         country_schools["name"] = country_schools["name"].astype(str).str.replace(" college","")
         country_schools["name"] = country_schools["name"].astype(str).str.replace(" College","")
+
         country_data = pd.read_excel(os.path.join(
-                                incoming_data_path,
-                                "schools",
-                                "schools_data.xlsx"),
-                            sheet_name=country)
+                                    incoming_data_path,
+                                    "schools",
+                                    "schools_data.xlsx"),
+                                sheet_name=country)
         country_data["School Name"] = country_data["School Name"].astype(str).str.replace(" School","")
         country_data["School Name"] = country_data["School Name"].astype(str).str.replace(" school","")
         country_data["School Name"] = country_data["School Name"].astype(str).str.replace(" college","")
         country_data["School Name"] = country_data["School Name"].astype(str).str.replace(" College","")
-        for st in country_schools.itertuples():
-            nearest_id, nearest_school, nearest_district,match = nearest_name(st,country_data,'School Name')
-            school_matches.append((st.node_id,st.name,st.school_district,nearest_id,nearest_school,nearest_district,match)) 
+        district_data = country_data.groupby(["District"])["Enrollment"].sum().reset_index()
+        district_data["iso_code"] = country
+        school_year = country_data["Year"].values[0]
+        all_districts.append(district_data.copy())
+        country_schools["year"] = school_year
+        if country != "DMA":
+            school_matches = []
+            for st in country_schools.itertuples():
+                nearest_id, nearest_school, nearest_district, enrollment, match = nearest_name(st,country_data,'School Name')
+                if match > match_threshold and st.school_district == nearest_district:
+                    school_matches.append((st.node_id,st.name,st.school_district,st.area,
+                            nearest_id,nearest_school,nearest_district,enrollment,match)) 
+                # school_matches.append((st.node_id,st.name,st.school_district,nearest_id,nearest_school,nearest_district,match))    
+            school_matches = pd.DataFrame(school_matches,
+                                columns=["node_id","name",
+                                        "school_district","area","ID",
+                                        "School Name","District",
+                                        "Enrollment","Match"])
+            school_matches["total_areas"] = school_matches.groupby(["ID","School Name"])["area"].transform('sum')
+            school_matches["assigned_students"] = school_matches["Enrollment"]*school_matches["area"]/school_matches["total_areas"]
+            school_matches["assigned_students"] = school_matches["assigned_students"].round(0)
+            district_matches = school_matches.groupby(["District"])["assigned_students"].sum().reset_index()
+            remaining_schools = country_schools[~(country_schools["node_id"].isin(school_matches["node_id"].values.tolist()))]
+            country_schools = pd.merge(country_schools,school_matches[["node_id","assigned_students"]],how="left",on=["node_id"])
+            all_schools.append(country_schools[country_schools["node_id"].isin(school_matches["node_id"]).values.tolist()])
 
-    school_matches = pd.DataFrame(school_matches,columns=["node_id","name","school_district","ID","School Name","District","Match"])
-    school_matches.to_csv(os.path.join(
-                        incoming_data_path,
-                        "schools",
-                        "schools_matches.csv"),index=False)
+            district_data = district_data.set_index("District")
+            district_matches = district_matches.set_index("District")
+            district_data["Enrollment"] = district_data["Enrollment"].sub(
+                                                                district_matches["assigned_students"],
+                                                                axis='index',fill_value=0
+                                                                )
+            district_data = district_data.reset_index()
+        else:
+            remaining_schools = country_schools.copy()
+
+        remaining_schools = pd.merge(remaining_schools,district_data,
+                                how="left",left_on=["school_district"],
+                                right_on=["District"]).fillna(0)
+        remaining_schools["total_areas"] = remaining_schools.groupby(["District"])["area"].transform('sum')
+        remaining_schools["assigned_students"] = remaining_schools["Enrollment"]*remaining_schools["area"]/remaining_schools["total_areas"]
+        remaining_schools["assigned_students"] = remaining_schools["assigned_students"].round(0)
+        remaining_schools["assigned_students"][remaining_schools["assigned_students"] < 0] = 0
+        # remaining_schools.drop(["District","Enrollment"],axis=1,inplace=True)
+        all_schools.append(remaining_schools)
+
+    all_schools = pd.concat(all_schools,axis=0,ignore_index=True)
+    schools = pd.merge(schools,all_schools[["node_id","school_district","assigned_students","year"]],how="left",on=["node_id"])
+
+    # check the totals at the district level
+    district_totals = schools.groupby(["iso_code","school_district"])["assigned_students"].sum().reset_index()
+    all_districts =  pd.concat(all_districts,axis=0,ignore_index=True)
+    district_totals = pd.merge(district_totals,
+                        all_districts,how="left",
+                        left_on=["iso_code","school_district"],right_on=["iso_code","District"])
+    
+    schools = gpd.GeoDataFrame(
+                        schools,
+                        geometry="geometry",
+                        crs=f"EPSG:{caribbean_epsg}")
+    schools.to_file(os.path.join(
+                    processed_data_path,
+                    "infrastructure/social",
+                    "education.gpkg"),
+                    layer="areas",driver="GPKG")
 
 
 if __name__ == '__main__':
